1588b13a51837dba49ba205ae53606e7ba49df68
Mateusz Sagan <m.sagan@binarapps.com>
2015-06-24 21:03:03 +0200

    Admin field for team

--- app/controllers/admin/teams_controller.rb
+++ app/controllers/admin/teams_controller.rb
@@ -69,6 +69,6 @@ class Admin::TeamsController < AdminController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
-      params.require(:team).permit(:name, :history, :situation_description)
+      params.require(:team).permit(:name, :history, :situation_description, :user_id)
     end
 end
