5754d0e426b14a1b34d175881b436e0834fa5034
Mateusz Sagan <m.sagan@binarapps.com>
2016-01-21 21:54:06 +0100

    fix

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module ApkaHarcerze
   class Application < Rails::Application
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
     config.autoload_paths += %W["#{config.root}/app/validators/"]
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
