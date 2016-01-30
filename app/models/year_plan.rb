5c47cb6a53ca1956d2cac8fa28f3fe66e788335e
Mateusz Sagan <m.sagan@binarapps.com>
2016-01-30 21:45:24 +0100

    user index change

--- app/models/year_plan.rb
+++ app/models/year_plan.rb
@@ -10,6 +10,9 @@ class YearPlan < ActiveRecord::Base
       badge[1].map!{ |b| [b, b.team_trial_count(self.team)] }
       badge[1].sort_by{ |b| b.reverse }
     end
+    plan_badges.each do |arr|
+      arr[1].sort! {|a,b| b[1] <=> a[1]}
+    end
   end
 
 end
